6f19809c232b65e5f25fc19c856bca542c168106
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-04 19:26:47 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array >> element
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
