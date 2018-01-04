e4f8d4781e8e080c3de79377d216ca27b4f73333
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-04 19:27:34 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
